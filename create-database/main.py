96050b8cfa3015671980c3f480293fa575049465
jesus andres zini <jesusandreszini14@gmail.com>
2023-05-29 17:15:51 -0300

    working on create database

--- create-database/main.py
+++ create-database/main.py
@@ -1,25 +1,46 @@
+import os
 import psycopg2
 
+# Retrieve environment variables
+host = os.environ.get("DB_HOST")
+database = os.environ.get("DB_NAME")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+new_database = os.environ.get("NEW_DB_NAME")
+
+# Connect to the PostgreSQL database
 conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="tu_usuario",
-    password="tu_contraseña"
+    host=host,
+    database=database,
+    user=user,
+    password=password
 )
 
+# Check if the new_database database exists
 cursor = conn.cursor()
-cursor.execute("CREATE DATABASE zini_system;")
+cursor.execute(f'SELECT 1 FROM pg_catalog.pg_database WHERE datname = \'{new_database}\'')
+exists = cursor.fetchone()
+
+if not exists:
+    # Create the new_database database
+    cursor.execute(f'CREATE DATABASE {new_database};')
+    print(f'Created {new_database} database.')
+else:
+    print(f'{new_database} database already exists.')
+
 conn.commit()
 cursor.close()
 conn.close()
 
+# Connect to the new_database
 conn = psycopg2.connect(
-    host="localhost",
-    database="zini_system",
-    user="tu_usuario",
-    password="tu_contraseña"
+    host=host,
+    database=new_database,
+    user=user,
+    password=password 
 )
 
+# Execute SQL commands from a file
 cursor = conn.cursor()
 sql_file = "./tables.sql" 
 
